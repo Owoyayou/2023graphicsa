4b00d7db3ad5c57977c670dcc9329811068664a7
Owoyayou <9823052ak@gmail.com>
2023-05-27 02:31:15 +0800

    week11

---
+++ week11/week11-3_PlaySound.cpp
@@ -0,0 +1,21 @@
+///Week11-3_PlaySound.cpp 可是它卻不能執行 因為找不到 PlaySoundA
+///C:\Users\Administrator\Desktop\week11-3_PlaySound.o:week11-3_PlaySound.cpp:(.text+0x2e):
+///  undefined reference to `PlaySoundA@12'
+///PlaySoundA() 在 winmm 裡面 ，手動把 winmm 加到 lib
+/// 比較: OpenCV 要 3咒語, GLUT 要 5咒語。 PlaySound 要 1咒語
+/// Setting-Compiler > Linker Settings > 加上winmm
+
+
+#include <windows.h>
+int main()
+{                         ///要等待同步 SND_SYNC, 不然會瞬間結束
+    PlaySound("do_re_mi\\do.wav", NULL, SND_SYNC);
+    PlaySound("do_re_mi\\do.wav", NULL, SND_SYNC);
+    PlaySound("do_re_mi\\do.wav", NULL, SND_SYNC);
+    PlaySound("do_re_mi\\re.wav", NULL, SND_SYNC);
+    ///PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\mi.wav", NULL, SND_SYNC);
+}
+///Build log 會寫: Checking for existence: C:\Users\Administrator\Desktop\week11-3_PlaySound.exe
+///Executing: '"C:\Program Files (x86)\CodeBlocks/cb_console_runner.exe" "C:\Users\Administrator\Desktop\week11-3_PlaySound.exe"' (in 'C:\Users\Administrator\Desktop')
+/// 它的執行目錄在 C:\Users\Administrator\Desktop
+/// 我們的 do.wav re.wav mi.wav 在相對目錄
